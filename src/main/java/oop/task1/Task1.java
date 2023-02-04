31f167e91e5ce08afd2b73fc17ca76f4538c7a1f
Arafat <sakib15-5422@diu.edu.bd>
2023-02-04 19:15:04 +0600

    "Task1"

---
+++ src/main/java/oop/task1/Task1.java
@@ -0,0 +1,14 @@
+
+package oop.task1;
+
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Arafat Rahman");
+        System.out.println("ID: 221-15-5422");
+        System.out.println("Address: Mohammadpur");
+        System.out.println("Department: BSc in CSE");
+        System.out.println("Favorite Game: Football");
+    }
+}
